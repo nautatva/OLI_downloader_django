6092601412fe35e2456b9aa39f3179e8cd109550
nautatva <nnautatva@gmail.com>
2020-01-31 20:56:37 +0530

    using import-export for model mass actions

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import language, video
 # Register your models here.
 
-admin.site.register(language)
-admin.site.register(video)
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class LanguageResource(resources.ModelResource):
+
+    class Meta:
+        model = language
+
+
+class VideoResource(resources.ModelResource):
+
+    class Meta:
+        model = video
+
+
+class LanguageAdmin(ImportExportModelAdmin):
+    resource_class = LanguageResource
+
+
+class VideoAdmin(ImportExportModelAdmin):
+    resource_class = VideoResource
+
+
+admin.site.register(language, LanguageAdmin)
+admin.site.register(video, VideoAdmin)
